e2b9779fc9dfbdf417968b2c2726f697c05ed25c
Anastasiya Haiduk <anastasiagajduk24@gmail.com>
2026-05-05 14:00:22 +0200

    added new context for user page

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,34 @@
+import { createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [weight, setWeight] = useLocalStorage("userWeight", 60);
+  const [height, setHeight] = useLocalStorage("userHeight", 167);
+  const [age, setAge] = useLocalStorage("userAge", 18);
+  const [gender, setGender] = useLocalStorage("userGender", "female");
+  const [activityLevel, setActivityLevel] = useLocalStorage(
+    "userActivityLevel",
+    "moderate",
+  );
+
+  return (
+    <UserContext.Provider
+      value={{
+        weight,
+        setWeight,
+        height,
+        setHeight,
+        age,
+        setAge,
+        gender,
+        setGender,
+        activityLevel,
+        setActivityLevel,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { FoodProvider } from "./context/FoodContext.jsx";
+import { UserProvider } from "./context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <FoodProvider>
-      <App />
-    </FoodProvider>
+    <UserProvider>
+      <FoodProvider>
+        <App />
+      </FoodProvider>
+    </UserProvider>
   </StrictMode>,
 );
